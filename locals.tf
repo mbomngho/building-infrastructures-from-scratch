2c8af8ed90837fc72e98a19b29b811e3022e2c45
lionel <lionelngaiwoh@yahoo.com>
2022-07-31 22:10:49 -0400

    updated code

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 
-locals {
-  vpc_id = aws_vpc.Kojitechsvpc.id
-  az_pub = "us-east-1b"
-  az_priv = "us-east-1a"
-}
+# locals {
+#   vpc_id = aws_vpc.Kojitechsvpc.id
+#   az_pub = "us-east-1b"
+#   az_priv = "us-east-1a"
+# }
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 
-output "public_ip" {
-    description = "public ip"
-  value = aws_instance.web.public_ip
-}
+# output "public_ip" {
+#     description = "public ip"
+#   value = aws_instance.web.public_ip
+# }
